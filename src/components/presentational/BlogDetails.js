6300f962e8f7a1fe8ac76d85f8547c84d0ea5e40
Liam Wohlfart <liam.wohlfart0@gmail.com>
2020-09-08 11:27:01 +0200

    Add form for editing

--- src/components/presentational/BlogDetails.js
+++ src/components/presentational/BlogDetails.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 import Loading from "./Loading";
 
 class BlogDetails extends Component {
   state = {
     blog: null,
+    edit: false,
   };
 
   async componentDidMount() {
@@ -18,6 +20,46 @@ class BlogDetails extends Component {
     this.setState({ blog });
   }
 
+  handleClick = async () => {
+    const { id } = this.props.match.params;
+
+    await fetch(`http://localhost:8080/api/blog/${id}`, {
+      method: "DELETE",
+    });
+
+    this.props.history.push("/blog");
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { id } = this.props.match.params;
+
+    const response = await fetch(`http://localhost:8080/api/blog/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(this.state.blog),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const blog = await response.json();
+
+    this.setState({ blog, edit: false });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      blog: {
+        ...this.state.blog,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  handleEdit = () => this.setState({ edit: !this.state.edit });
+
+  // TODO
+  renderBlog() {}
+
   render() {
     if (!this.state.blog) {
       return <Loading />;
@@ -25,14 +67,56 @@ class BlogDetails extends Component {
 
     const { title, text } = this.state.blog;
 
+    if (!this.state.edit) {
+      return (
+        <article>
+          <h1>{title}</h1>
+          <p className="mt-5">{text}</p>
+          <LinkContainer to="/blog">
+            <Button className="mr-2">Zurück</Button>
+          </LinkContainer>
+          <Button variant="danger" onClick={this.handleClick} className="mr-2">
+            Löschen
+          </Button>
+          <Button variant="warning" onClick={this.handleEdit}>
+            Edit
+          </Button>
+        </article>
+      );
+    }
+
     return (
-      <article>
-        <h1>{title}</h1>
-        <p className="mt-5">{text}</p>
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Group>
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            value={this.state.blog.title}
+            name="title"
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Text</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows="5"
+            value={this.state.blog.text}
+            name="text"
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Button type="submit" variant="success" className="mr-2">
+          Speichern
+        </Button>
         <LinkContainer to="/blog">
-          <Button>Zurück</Button>
+          <Button variant="danger" className="mr-2">
+            Abrechen
+          </Button>
         </LinkContainer>
-      </article>
+        <Button variant="warning" onClick={this.handleEdit}>
+          Edit
+        </Button>
+      </Form>
     );
   }
 }
