c7742a8bb3957fa332ff15552aef2bdbfd5f16fa
Liam Wohlfart <liam.wohlfart0@gmail.com>
2020-09-08 10:00:11 +0200

    Shorter BlogList

--- src/components/presentational/BlogList.js
+++ src/components/presentational/BlogList.js
@@ -3,14 +3,11 @@ import Col from "react-bootstrap/Col";
 
 import BlogCard from "./BlogCard";
 
-const BlogList = ({ blogs }) => {
-  const blogList = blogs.map((blog) => (
+const BlogList = ({ blogs }) =>
+  blogs.map((blog) => (
     <Col lg="4" className="mt-4" key={blog._id}>
       <BlogCard blog={blog} />
     </Col>
   ));
-
-  return blogList;
-};
 
 export default BlogList;
